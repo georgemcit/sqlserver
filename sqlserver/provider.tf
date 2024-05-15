1e406a7020ae420514d2cf17f9054972b50419a0
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-15 18:36:18 -0400

    Update provider.tf

--- sqlserver/provider.tf
+++ sqlserver/provider.tf
@@ -18,15 +18,4 @@ provider "azurerm"{
   client_secret=var.client_secret
   tenant_id=var.tenant_id
 }
-variable "subscription_id"{
-  type=string
-}
-variable "client_id"{
-  type=string
-}
-variable "client_secret"{
-  type=string
-}
-variable "tenant_id"{
-  type=string
-}
+
