6b60f24f595de0a8711cca6e289093c1fd8f1d4f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-15 19:29:51 -0400

    Update local.tf

--- sqlserver/local.tf
+++ sqlserver/local.tf
@@ -1,1 +1,12 @@
-
+locals{
+  sql_server=[for f in fileset("${path.module}/${var.sqlserver}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.sqlserver}/${f}"))]
+  sql_server_list = flatten([
+    for app in local.sql_server: [
+      for sqlserver in try(app.listofsqlserver, []) :{
+        name=linuxapps.mssqlserver
+        version=sqlserver.version
+        minimum_tls_version=sqlserver.minimum_tls_version 
+      }
+    ]
+])
+}
