889096887e74df65da5fff7b6517d572a0164c69
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-15 19:40:07 -0400

    Update local.tf

--- sqlserver/local.tf
+++ sqlserver/local.tf
@@ -1,12 +1,30 @@
 locals{
-  sql_server=[for f in fileset("${path.module}/${var.sqlserver}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.sqlserver}/${f}"))]
-  sql_server_list = flatten([
-    for app in local.sql_server: [
-      for sqlserver in try(app.listofsqlserver, []) :{
-        name=linuxapps.mssqlserver
-        version=sqlserver.version
-        minimum_tls_version=sqlserver.minimum_tls_version 
+  mssql_server=[for f in fileset("${path.module}/${var.sqlserver}", "[^_]*.yaml") : yamldecode(file("${path.module}/${var.sqlserver}/${f}"))]
+  mssql_server_list = flatten([
+    for app in local.mssql_server: [
+      for mysqlserver in try(app.listofmysqlserver, []) :{
+        name=mysqlserver.mssqlserver
+        version=mysqlserver.version
+        minimum_tls_version=mysqlserver.minimum_tls_version 
       }
     ]
 ])
+}
+resource "azurerm_mssql_server" "azuresqlserver" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.databaserg.name
+  location                     = azurerm_resource_group.databaserg.location
+  version                      = each.value.version
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = each.value.minimum_tls_version
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    Environment = var.environment_tag
+  }
 }
