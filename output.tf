4f15023af384c8edb409500dcfc56d013edb9cd0
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-20 01:07:34 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,6 +9,10 @@ variable "sqlserver"{
  type=string
  default="sqlserver"
 }
+variable "version"{
+ type=number
+ default="12.0"
+}
 /*
 variable "environment_tag"{
  type=string
@@ -17,10 +21,6 @@ default="production"
 variable "sql_server_name"{
  type=string
 default="mssqlserver"
-}
-variable "sql_version"{
- type=string
- default="12.0"
 }
 variable "minimum_tls_version"{
  type=string
