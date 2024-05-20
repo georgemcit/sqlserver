9b85a1e5524da15ed9ddedf6896514bfbc7b63c4
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-20 00:32:25 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,31 +1,39 @@
 locals{
-  mssqlserver=[for f in fileset("${path.module}/sqlserver", "[^_]*.yaml") : yamldecode(file("${path.module}/sqlserver/${f}"))]
-  mssqlserverlist = flatten([
-    for app in local.mssqlserver : [
-      for mssqlserver in try(app.listofmssqlserver, []) :{
-        name=mssqlserver.mssqlservername
+  mssql_server=[for f in fileset("${path.module}/sqlserver", "[^_]*.yaml") : yamldecode(file("${path.module}/sqlserver/${f}"))]
+  mssql_server_list = flatten([
+    for app in local.mssql_server : [
+      for mssqlservers in try(app.listofmssqlserver, []) :{
+        name=linuxapps.name
+        version=mssqlservers.version
+        minimum_tls_versione=mssqlservers.minimum_tls_version
 
       }
     ]
 ])
 }
-
 resource "azurerm_resource_group" "databaserg" {
   name     = "database-rg"
   location = "West Europe"
 }
-
-resource "azurerm_mssql_server" "azuresqlserver" {
+resource "azurerm_mssql_server_plan" "george" {
   for_each            ={for sp in local.mssqlserver: "${sp.name}"=>sp }
   name                         = each.value.name
-  resource_group_name          = azurerm_resource_group.databaserg.name
-  location                     = azurerm_resource_group.databaserg.location
-  version                      = each.value.version
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  version             = each.value. version
+  minimum_tls_versione            = each.value.minimum_tls_version
+}
+
+resource "azurerm_mssql_server" "georgeibrahim" {
+  for_each            = azurerm_service_plan.george
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
   administrator_login          = var.administrator_login
   administrator_login_password = var.administrator_login_password
-  minimum_tls_version          = each.value.minimum_tls_version
+  service_plan_id     = each.value.id
 
-  azuread_administrator {
+azuread_administrator {
     login_username = "AzureAD Admin"
     object_id      = "00000000-0000-0000-0000-000000000000"
   }
@@ -34,4 +42,4 @@ resource "azurerm_mssql_server" "azuresqlserver" {
     environment = "production"
   }
 }
-
+ 
