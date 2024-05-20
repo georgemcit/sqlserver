b65fe92240e55889cdb92295bcfa2db4e3acad1b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-20 02:28:04 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ locals{
   my_sql_app_list = flatten([
     for app in local.my_sql_app: [
       for serverapps in try(app.listofmysqlserver, []) :{
-        name=serverapps.servername
+        name=serverapps.name
       }
     ]
 ])
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "databaserg" {
 
 resource "azurerm_mssql_server" "azuresqlserver" {
   for_each            ={for sp in local.my_sql_app_list: "${sp.name}"=>sp }
-  servername                = each.value.servername
+  name                = each.value.name
   resource_group_name          = azurerm_resource_group.databaserg.name
   location                     = azurerm_resource_group.databaserg.location
   version                      = "12.0"
