8dd6406d38c6dd4e64e42a6f2327ae2ce3744112
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-20 01:23:50 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "databaserg" {
   location = "West Europe"
 }
 resource "azurerm_mssql_server" "george" {
-  for_each            ={for mssqlserver in local.mysql_server: "${mssqlserver.name}"=>mysqlserver }
+  for_each            ={for mssqlserver in local.mysql_server: "${mssqlserver.name}"=>mssqlserver }
   name                = each.value.name
   resource_group_name = azurerm_resource_group.databaserg.name
   location            = azurerm_resource_group.databaserg.location
