e698e70c82da297d6c60efebb8fd4a25612ab9c3
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-20 01:36:32 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,30 +1,18 @@
-locals{
-  mysql_server=[for f in fileset("${path.module}/sqlserver", "[^_]*.yaml") : yamldecode(file("${path.module}/sqlserver/${f}"))]
-  mysql_server_list = flatten([
-    for app in local.mysql_server : [
-      for mysqlapps in try(app.listofmysqlserver, []) :{
-        name=mysqlapps.name
-
-      }
-    ]
-])
-}
 resource "azurerm_resource_group" "databaserg" {
   name     = "database-rg"
   location = "West Europe"
 }
-resource "azurerm_mssql_server" "george" {
-  for_each            ={for sp in local.mysql_server: "${sp.name}"=>sp }
-  name                = each.value.name
-  resource_group_name = azurerm_resource_group.databaserg.name
-  location            = azurerm_resource_group.databaserg.location
+
+resource "azurerm_mssql_server" "azuresqlserver" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.databaserg.name
+  location                     = azurerm_resource_group.databaserg.location
+  version                      = "12.0"
   administrator_login          = var.administrator_login
   administrator_login_password = var.administrator_login_password
-  version             = var.version_number
-  minimum_tls_version           = "1.2"
+  minimum_tls_version          = "1.2"
 
-
-azuread_administrator {
+  azuread_administrator {
     login_username = "AzureAD Admin"
     object_id      = "00000000-0000-0000-0000-000000000000"
   }
@@ -33,4 +21,3 @@ azuread_administrator {
     environment = "production"
   }
 }
- 
