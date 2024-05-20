6cef9f2a0273d1acadd03dde3c2cae6946cf7d53
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-20 01:47:57 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 locals{
-  my-sql-app=[for f in fileset("${path.module}/sqlserver", "[^_]*.yaml") : yamldecode(file("${path.module}/sqlserver/${f}"))]
+   my-sql-app=[for f in fileset("${path.module}/sqlserver", "[^_]*.yaml") : yamldecode(file("${path.module}/sqlserver/${f}"))]
    my-sql-app_list = flatten([
     for app in local.sql_app: [
       for mysqlapps in try(app.listofmysqlserver, []) :{
